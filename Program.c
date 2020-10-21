79cff65466c900033abd776ce9cd85c0ba59eb10
ignesh1234 <73224046+ignesh1234@users.noreply.github.com>
2020-10-21 14:42:11 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    char name,country,skill;
+    char name,country,skill,class;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
     scanf("%d",&name);
